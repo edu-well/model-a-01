fed1b560563d1b1afde43a0d49d682934fbc0792
edu-well <108446261+edu-well@users.noreply.github.com>
2025-04-14 11:25:47 +0200

    Update datadsdc-1.js

--- data/js/database/dataphysique/datadsdc-1.js
+++ data/js/database/dataphysique/datadsdc-1.js
@@ -1,44 +1,611 @@
 const pdfDatabase = [
     { 
-        name: "Maths Espace", 
-        author: "Eduwell",
-        description: "Cours complet avec exercices Espace .",
-        url: "https://example.com/pdfs/ahmed_math.pdf", 
-        keywords: ["ahmed", "math", "algèbre" ,"espace"] 
+        name: "Devoir de controle 1 2012", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
     },
+
     { 
-        name: "Physique", 
-        author: "Pilot Sousse",
-        description: "Force Mécanique - équation différentielle.",
-        url: "https://example.com/pdfs/ahmed_physique.pdf", 
-        keywords: ["force ","mecanique", "physique", "equation différentielle"] 
+        name: "Devoir de controle 1 2012", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1JAi-qrHN6aZnughOZt7GM9V-4A0JDtAO/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
     },
+
+
+
+
+
+
+
+
     { 
-        name: "Algèbre", 
-        author: "Pilot Tunis",
-        description: "des Problème Mathématiques avec correction.",
-        url: "https://example.com/pdfs/algebre_lineaire.pdf", 
-        keywords: ["math", "algèbre","exponential"] 
+        name: "Devoir de controle 1 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1XJqZZV6XCGzu7mEa4r4wtarnJhR579yc/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
     },
+
     { 
-        name: "Calcul Différentiel", 
-        author: "Jean Dupont",
-        description: "Dérivées et optimisation pour étudiants en sciences.",
-        url: "https://example.com/pdfs/calcul_diff.pdf", 
-        keywords: ["math", "calcul"] 
+        name: "Devoir de controle 1 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1-Xzf-BZ2Ec_bvGp2SBU6AcfU_4XexjgP/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
     },
+
+
+
+
+
+
+
+
+
+
+
+
+
     { 
-        name: "Histoire Moderne", 
-        author: "Pilot Sfex",
-        description: "Analyse de la période moderne en Europe.",
-        url: "https://example.com/pdfs/histoire_moderne.pdf", 
-        keywords: ["histoire", "moderne"] 
+        name: "Devoir de controle 1 2015", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/13t2dXMP08jtqF-GJWu1-IfbE7sZQVsRM/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
     },
+
     { 
-        name: "Chimie Organique", 
-        author: "Eduwell",
-        description: "Principes fondamentaux avec expériences pratiques.",
-        url: "https://example.com/pdfs/chimie_organique.pdf", 
-        keywords: ["chimie", "organique"] 
-    }
+        name: "Devoir de controle 1 2015", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1n6KQ35ZAVip2uf3GDVOAt6uq3GaeFVgH/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+
+
+
+
+
+
+    { 
+        name: "Devoir de controle 1 2016", 
+        author: "Direction régionale de Ben Arous - 2016",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1-jXEXFwntDvKswrTgPfYfG30iJIcXLwC/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2016", 
+        author: "Direction régionale de Ben Arous - 2016",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1xmoiMmJ424gFLy4Q7B6CJDHVjWdM2eEp/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+
+
+
+
+    { 
+        name: "Devoir de controle 1 2017", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/177NpDsNZHbjSDYejTMEtxGLYHOWqWJvj/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2017", 
+        author: "Direction régionale de Ben Arous - 2017",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1jmY9iBETiDBapr7mhAvNjdG_JutftqF2/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+    { 
+        name: "Devoir de controle 1 2018", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1bUCHUEqiYBRogDE5DBiHxVhJe5mKBlId/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2018", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1B-yokSZ_OP_kdemFz3nh1-wKcTHZvxNJ/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+    { 
+        name: "Devoir de controle 1 2019", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1RUIEahG0yigKUsZXQ2U8ojJFneuQdnx0//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2019", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/11Rj2rvcX7_rt2XlfOE4QNGa1mpwzf69M/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+    { 
+        name: "Devoir de controle 1 2020", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2020", 
+        author: "Direction régionale de Ben Arous ",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1lP-JLkHzXxOiTQezoBjb-eYe01m1VIQ/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+    { 
+        name: "Devoir de controle 1 2021", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1LL2ilCD2zezZcEec5eeOLFb1f7K1RBT6/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 1 2021", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1x8XJWJqaNMhlhQKZqLjqXZCeZa0sXNis/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    { 
+        name: "Devoir de synthese 1 2012", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/15GLpeiQQU9SemH4oZ6rFtwTbo7LOHIJj/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de synthese 1 2012", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1FyrOqIVZrqq2clPSMqcRBQscSmC3zt9L/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+
+    { 
+        name: "Devoir de synthese 1 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1Q5Br4jGQb1T2drQwWQ37Bp0-GeifJoOA/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de synthese 1 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/167zpIWdoNOcOALbFB7v5eVk7yL9NmpPl/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+
+    { 
+        name: "Devoir de synthese 1 2015", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/17_OvMxT-yYAsXhPQK_n1N94aItOf38xc/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de synthese 1 2015", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1pq48xKUgDAlDTmUV2p-EADZbkcmkHm7B/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+
+    { 
+        name: "Devoir de synthese 1 2020", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/13gX-wa71RN5U4wDYQvf38omC-2zAO4Bt/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de synthese 1 2020", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1YiyAYu2oy_ZJXvrGGIvJZ87k6YJpSjPt/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+    { 
+        name: "Devoir de synthese 1 2021", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/17kCSdRD2dS8ZZfqDkzsGeqKDefDi92Pb/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de synthese 1 201", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1mlAphNpgpbweaBDVFmyavoPB-intlRsr/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    { 
+        name: "Devoir de controle 2 2013", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1SBQhJ8_xQIA64RgAon5Fdm1VeBfGrRKJ//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2013", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1RXIOBy4Z8jUMiC9RWWdY7AxnkEVJBfS8/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+    
+    
+    { 
+        name: "Devoir de controle 2 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1JM-1BEf-Yvy5VBg_bEgwyPtfwRrA2Vm4//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2014", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/189G2nCKHMrWd8XtPgFVIkzW5wtz8MBjJ/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+
+    
+    
+    { 
+        name: "Devoir de controle 2 2015", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1HRzHyOw6RNZ8AkaGZvgCtsIu8MVuWFue//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2015", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1qmXCw098GMkTZL3tTrGHq--GlDy3impg/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+
+
+    
+    
+    { 
+        name: "Devoir de controle 2 2016", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1UY4niX9RO-0WrTYBCv6ofRItRZRXoy3E//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2016", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1D29LRS92Eo3P_yIZxibnHCeKwsSDurzc/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+
+    
+    
+
+    
+
+
+    
+    { 
+        name: "Devoir de controle 2 2020", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1P--nuQq1yDDWSq889FUai_LazJwAiKWi//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2020", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1lXWxp_fItM5oIznmN_GvEonaq5HpL7BU/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+    
+    { 
+        name: "Devoir de controle 2 2021", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1JlFhm5EAS80b8hhkQgiAH0OIENnxZbN6//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2021", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1f3ESSFIUictkxAHDwiQ7u2ZbY5yS_6I6/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+
+
+
+    
+    { 
+        name: "Devoir de controle 2 2022", 
+        author: "Direction régionale de Ben Arous",
+        description: "Enonce Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1Cbe-LS6-XE35FkHLsbKKp-upoUkaBziX//preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+    { 
+        name: "Devoir de controle 2 2022", 
+        author: "Direction régionale de Ben Arous",
+        description: "Corraction Sciences physiques  ",
+        url: "https://drive.google.com/file/d/1XX9TNmaBqA2tDbd3uL8VSD5DQ2dr3dLE/preview", 
+        
+        keywords: ["controle 1", "controle1","c1","physique", "bac" ,"rc","2012"] 
+    },
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
